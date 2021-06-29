964c5ae59ff71be4102c006291feb09dc755f943
Varsha Dantu <66246896+VarshaDantu@users.noreply.github.com>
2021-06-29 09:42:44 +0530

    Update generated_plugin_registrant.dart

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -10,7 +10,7 @@ import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-// ignore: public_member_api_docs
+// ignore :public_member_api_docs
 void registerPlugins(Registrar registrar) {
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
